9bf7b8b14529aa2eacfa3936079918e13b6238e1
Moura, Heytor C <heytor.c.moura@accenture.com>
2021-01-20 18:12:55 -0300

    Fixed show document status on overview page

--- src/styles/components/Text/index.tsx
+++ src/styles/components/Text/index.tsx
@@ -5,3 +5,27 @@ export const Title = styled.h3`
   margin-bottom: 30px;
   font-weight: 600;
 `;
+
+export const ElegibleLabel = styled.span`
+  color: var(--success);
+`;
+
+export const NotElegibleLabel = styled.span`
+  color: var(--danger);
+`;
+
+export const HighComplexityLabel = styled.span`
+  color: var(--danger);
+`;
+
+export const MediumComplexityLabel = styled.span`
+  color: var(--warning);
+`;
+
+export const LowerComplexityLabel = styled.span`
+  color: var(--success);
+`;
+
+export const NoComplexityLabel = styled.span`
+  color: var(--gray);
+`;
