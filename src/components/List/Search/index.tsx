2c3fcf62791388f44ea0484ea8ed0fd73402776f
brunogcp <brunogcpereira@gmail.com>
2021-07-02 14:21:23 -0300

    Initial Commit

---
+++ src/components/List/Search/index.tsx
@@ -0,0 +1,46 @@
+import React, { ChangeEvent } from 'react';
+import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput } from '@material-ui/core';
+import { SearchOutlined } from '@material-ui/icons';
+
+import { FormSearch, ButtonStyle } from './styles';
+
+interface SearchProps {
+  onChangeInput: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  handleButton: () => void;
+  value?: string;
+  buttonTitle?: string;
+  inputPlaceholder?: string;
+}
+
+const Search = ({ value, onChangeInput, buttonTitle, handleButton, inputPlaceholder }: SearchProps) => {
+  return (
+    <div>
+      <FormSearch noValidate autoComplete="off">
+        <FormControl variant="outlined" size="small" >
+          <InputLabel htmlFor="search-input" >{inputPlaceholder = inputPlaceholder}</InputLabel>
+          <OutlinedInput
+            id="search-input"
+            value={value}
+            type="search"
+            onChange={onChangeInput}
+            onKeyDown={(event: any) => event.keyCode === 13 && event.preventDefault()}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton aria-label="toggle password visibility" edge="end">
+                  <SearchOutlined />
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={390}
+          />
+        </FormControl>
+        {buttonTitle && (
+          <ButtonStyle onClick={handleButton}>{buttonTitle}</ButtonStyle>
+        )}
+
+      </FormSearch>
+    </div>
+  );
+}
+
+export default React.memo(Search);
