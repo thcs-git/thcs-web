7a6959a9a7066919e4139b4fc94832126448e308
Bruno Pereira <bruno.pereira@tascominformatica.com.br>
2021-07-20 11:12:45 +0000

    SOLL-290

--- src/pages/patient/form/index.tsx
+++ src/pages/patient/form/index.tsx
@@ -982,7 +982,7 @@ export default function PatientForm(props: RouteComponentProps<IPageParams>) {
                             renderInput={(params) => <TextField {...params} label="Área"  variant="outlined"  />}
                             size="small"
                             defaultValue={selectPatientArea()}
-                           // value={selectPatientArea()}
+                            value={selectPatientArea()}
                             onChange={(event:any, newValue) =>{
                               if(newValue){
                                 setState({...state,area_id:newValue._id})
