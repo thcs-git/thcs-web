2ce312b465a32908c27df0d1c2381f2ff1d415b0
victorrsouzas <victorrsouzas@gmail.com>
2021-08-02 13:58:23 -0300

    SOLLAR-105

--- src/pages/patient/list/index.tsx
+++ src/pages/patient/list/index.tsx
@@ -395,13 +395,13 @@ export default function PatientList() {
                   return (
                     <TableRow key={`patient_${index}`}>
                       <TableCell >
-                      <p>{patient?.started_at ? formatDate(patient?.started_at ?? "", "DD/MM/YYYY") : ""}</p>
+                      <p>{patient?.started_at ? formatDate(patient?.started_at ?? "", "DD/MM/YYYY") : "-"}</p>
                       </TableCell>
                       <TableCell align="center">
                         <p>{patient?._id}</p>
                       </TableCell>
                       <TableCell align="center">
-                        <p></p>
+                        <p>-</p>
                       </TableCell>
                       <TableCell align="center">
                         {typeof patient?.care_type_id === "object"
