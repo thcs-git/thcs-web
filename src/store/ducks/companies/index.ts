1aeef102002715db2e1934ad55a5527dde58ac0d
tarcisionet0 <tarcisio.neto@tascominformatica.com.br>
2021-08-11 14:13:07 -0300

    fix SOLLAR-29: Resolvido CEP - Empresas

--- src/store/ducks/companies/index.ts
+++ src/store/ducks/companies/index.ts
@@ -62,11 +62,12 @@ const reducer: Reducer<CompanyState> = (state = INITIAL_STATE, action) => {
     case CompanyTypes.LOAD_RESPONSE_ADDRESS:
       return {
         ...state,
+        success: true,
         loading: false,
         error: false,
         errorCep: false,
         data: {
-          ...state.data,
+          // ...state.data,
           address: {
             ...state.data.address,
             postal_code: action.payload.data.cep,
