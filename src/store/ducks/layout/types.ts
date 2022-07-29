6ee6a74bd2dbd6ca3dcb51ea74ff97cf38d03c4d
Tascom Informática <git@tascominformatica.com.br>
2022-07-29 19:56:00 +0000

    Hot-Fix: Apresentação
    Hot-Fix: Apresentação
    
    Pequenas correções para relalizar a apresentação

--- src/store/ducks/layout/types.ts
+++ src/store/ducks/layout/types.ts
@@ -2,42 +2,43 @@
  * Action types
  */
 export enum LayoutTypes {
-  LOAD_REQUEST = "@layout/LOAD_REQUEST",
-  LOAD_SUCCESS = "@layout/LOAD_SUCCESS",
-  LOAD_FAILURE = "@layout/LOAD_FAILURE",
+    LOAD_REQUEST = "@layout/LOAD_REQUEST",
+    LOAD_SUCCESS = "@layout/LOAD_SUCCESS",
+    LOAD_FAILURE = "@layout/LOAD_FAILURE",
 
-  CHANGE_MENU_SELECTED = "layout/CHANGE_MENU_SELECTED",
+    CHANGE_MENU_SELECTED = "layout/CHANGE_MENU_SELECTED",
 
-  CLEAN = "@layout/CLEAN",
+    CLEAN = "@layout/CLEAN",
 }
 
 /**
  * Data types
  */
 export interface MenuInterface {
-  icon: string;
-  id: number;
-  name: string;
-  slug?: string;
-  color: string;
-  modal?: any;
+    icon: string;
+    id: number;
+    name: string;
+    slug?: string;
+    color: string;
+    modal?: any;
 }
 
 export interface LayoutInterface {
-  token: string;
-  menu?: MenuInterface[];
-  rights?: string[];
-  integration?: string | null;
-  integration_name?: string | null;
-  menuSelected?: string;
+    token: string;
+    menu?: MenuInterface[];
+    rights?: string[];
+    integration?: string | null;
+    integration_name?: string | null;
+    menuSelected?: string;
+    time_zone?: string;
 }
 
 /**
  * State type
  */
 export interface LayoutState {
-  data: LayoutInterface;
-  loading: boolean;
-  error: boolean;
-  success: boolean;
+    data: LayoutInterface;
+    loading: boolean;
+    error: boolean;
+    success: boolean;
 }
